92bfb42dcb8086089bedf023e5ed32e4935bc6ba
Iva Boh <antutilo@gmail.com>
2023-11-18 19:22:40 +0200

    [IMP] school_lesson_6_2: all tasks done.

--- school_lesson_6_2/models/library_author.py
+++ school_lesson_6_2/models/library_author.py
@@ -2,20 +2,21 @@ from odoo import fields, models
 
 
 class LibraryAuthor(models.Model):
-    _name = 'library.author'
-    _description = 'Library Book Authors'
+    _name = "library.author"
+    _description = "Library Book Authors"
 
-    first_name = fields.Char(required=True)
-    last_name = fields.Char(required=True)
-    birth_date = fields.Date('Birthday')
+    first_name = fields.Char(required=True, translate=True)
+    last_name = fields.Char(required=True, translate=True)
+    birth_date = fields.Date("Birthday")
 
     def name_get(self):
-        return [(rec.id, "%s %s" % (
-            rec.first_name, rec.last_name)) for rec in self]
+        return [
+            (rec.id, "%s %s" % (rec.first_name, rec.last_name)) for rec in self
+        ]
 
     def action_delete(self):
         self.ensure_one()
-        self.check_access_rights('unlink')
+        self.check_access_rights("unlink")
         self.unlink()
 
     def _create_by_user(self, vals):
